ea7923566bc16203f663d054dc55316a65ab4ee3
NOURAHSIAK <146673842+NOURAHSIAK@users.noreply.github.com>
2024-08-15 00:37:29 +0100

    Update builtinfunc.py

--- builtinfunc.py
+++ builtinfunc.py
@@ -1,4 +1,19 @@
-mylist = ["osama", "ali", "adem"]
-x = map(lambda name: f"-{name.strip().capitalize()}-", mylist)
-for name in list(x):
-    print(name)
+# # def checkname(name):
+# #     if name.startswith("O"):
+# #         return name
+# mynames = ["Osama","ali", "Ons", "kais", "adem"]
+# myreturneddata = filter(lambda name: name.startswith("a"), mynames)
+# for person in myreturneddata:
+#     print(person)
+
+# import sys
+# sys.path.append(r"c:\games")
+# print(sys.path)
+# import elzero
+# print(dir(elzero))
+# elzero.sayhello("ahmed")
+# elzero.sayhi("ahmed")
+from elzero  import sayhello
+
+sayhello("ahmed")
+# elzero.sayhi("ahmed")
