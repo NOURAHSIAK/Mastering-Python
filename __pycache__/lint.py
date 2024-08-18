4c1a87b70f1c880c3e5df0cca873d2db2b1dfa5e
NOURAHSIAK <146673842+NOURAHSIAK@users.noreply.github.com>
2024-08-18 10:46:52 +0100

    Create lint.py

---
+++ __pycache__/lint.py
@@ -0,0 +1,12 @@
+"""
+this function to say hello
+to create func
+to salute
+"""
+
+def say_hello(name):
+    '''this function to salute person'''
+    x = "hello"
+    return f"{x} to {name}"
+
+say_hello("ahmed")
